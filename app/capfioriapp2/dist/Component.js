9fcce0aef0429b4b3a95fa577b119c7c94a391e1
Joy Saraswaty <joytcs>
2025-10-07 02:03:15 +0000

    7th Commit yaml change

---
+++ app/capfioriapp2/dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("capm.capfioriapp2.Component",{metadata:{manifest:"json"}})});
+//# sourceMappingURL=Component.js.map
